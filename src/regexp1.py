a600fa521257f2c20b2f958e23363b21052661bc
Evegenn <demerbrise@gmail.com>
2016-05-19 17:23:27 +0300

    working with regexp

---
+++ src/regexp1.py
@@ -0,0 +1,12 @@
+'''
+Created on 19 мая 2016 г.
+
+@author: Lartsev
+'''
+import re
+fo = open("mbox-short.txt")
+
+for line in fo:
+    line = line.rstrip()
+    if re.search('From ', line):
+        print(line)   
