cfa68bab9275abb828fe1039dbed6dda5c6b3521
Evegenn <demerbrise@gmail.com>
2016-05-19 16:51:03 +0300

    problem with missing file path has been solved

--- src/openFile.py
+++ src/openFile.py
@@ -1,5 +1,12 @@
 
+#if you run this program via python shell
 #file named "romeo.txt" must be placed in a folder that you use to run python shell
+#
+#if you run this program via PyDev
+#Choose "Python Run" and your run configuration from the left and then in the "Arguments"
+#tab in the right side, set your "working directory" as "other" with giving
+#"the path" on which you want to run your code.
+
 fo = open("romeo.txt", "r")
 
 count = 0
