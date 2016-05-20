79f769ecaabe5386430e0c5989bf2af630342a66
Evegenn <demerbrise@gmail.com>
2016-05-20 17:01:46 +0300

    found romeo.txt

--- src/HTTPRequest2.py
+++ src/HTTPRequest2.py
@@ -4,6 +4,7 @@ Created on 20 мая 2016 г.
 @author: Lartsev
 '''
 import urllib.request
-fhand = urllib.request.urlopen('https://wordpress.org/plugins/about/readme.txt')
+#fhand = urllib.request.urlopen('https://wordpress.org/plugins/about/readme.txt')
+fhand = urllib.request.urlopen('http://py4inf.com/code/romeo.txt')
 for line in fhand:
     print(line.strip())
