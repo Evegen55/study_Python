df7d6254bf9df2a17e5176244f1922be3f11aa43
Evegenn <demerbrise@gmail.com>
2016-05-20 17:11:07 +0300

    task under PR4E for week3 has been completed

---
+++ src/Task_Week_3.py
@@ -0,0 +1,19 @@
+'''
+Created on 20 мая 2016 г.
+
+@author: Lartsev
+'''
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.py4inf.com', 80))
+#see http://stackoverflow.com/questions/5471158/typeerror-str-does-not-support-the-buffer-interface
+#mysock.send(bytes('GET http://www.py4inf.com/code/demo/romeo.txt HTTP/1.0\n\n', 'UTF-8'))
+#mysock.send(bytes('GET http://www.dailytechinfo.org/infotech/8120-kompaniya-google-razrabotala-sobstvennyy-specializirovannyy-processor-prednaznachennyy-dlya-sistem-iskusstvennogo-intellekta.html HTTP/1.0\n\n', 'UTF-8'))
+mysock.send(bytes('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n', 'UTF-8'))
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data)
+mysock.close()
