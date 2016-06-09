dc7b9fbdbb01adc6f1933eda47f9f7689782570f
Evegenn <demerbrise@gmail.com>
2016-06-10 00:50:56 +0300

    В ответе используйте [ ] скобки для обозначения степени. Например, 3 в кубе = 3[3], 10 в кубе = 10[3].

---
+++ src/ListOfCubs.py
@@ -0,0 +1,17 @@
+import math
+def abc():
+    i = 100
+    b = 0;
+    c = 0;
+    d = 0;
+    cubs = 0;
+    while i < 1000:
+        a = str(i)
+        b = int(a[0])
+        c = int(a[1])
+        d = int(a[2])
+        cubs = (math.pow(b, 3) + math.pow(c, 3)+ math.pow(d, 3))
+        if (cubs == i):
+            print(i,"\t","b")
+        i = i + 1   
+abc()        
