bcedeee85eadb0f95604691b57b310aa1bcb3b42
Evegenn <demerbrise@gmail.com>
2016-06-15 02:13:28 +0300

    ‪#‎ЗадачкаОтДойче‬ Найдите все трехначные числа равные сумме кубов своих цифр

--- src/DeutscheBankTasks/ListOfCubs.py
+++ src/DeutscheBankTasks/ListOfCubs.py
@@ -12,6 +12,6 @@ def abc():
         d = int(a[2])
         cubs = (math.pow(b, 3) + math.pow(c, 3)+ math.pow(d, 3))
         if (cubs == i):
-            print(i,"\t","b")
+            print(i,"\t")
         i = i + 1   
 abc()        
