27af2acbe4141f3d3747755d82a86857816943c1
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-11-10 02:02:09 +0300

    successful

--- src/Udactiy/ud513/StackPracticeQuiz.py
+++ src/Udactiy/ud513/StackPracticeQuiz.py
@@ -29,11 +29,21 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        current = self.head
+        if current:
+            self.head = new_element
+            new_element.next = current
+        else:
+            self.head = new_element  
+        
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        current = self.head
+        if current:
+            self.head = None
+            self.head = current.next
+            return current        
 
 class Stack(object):
     def __init__(self,top=None):
@@ -41,11 +51,11 @@ class Stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)        
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.ll.delete_first()        
     
 # Test cases
 # Set up some Elements
