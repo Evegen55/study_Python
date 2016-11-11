d7b93839cd9996e416aaeec27943fb3dad040928
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-11-11 13:44:54 +0300

    The Fibonacci Sequence with recursion

--- src/Udactiy/ud513/Fibonacchi.py
+++ src/Udactiy/ud513/Fibonacchi.py
@@ -31,6 +31,24 @@ def getFib(position):
     return next;
 
 k = 0
-while k < 200:
+while k < 12:
     print getFib(k)
     k = k+1
+    
+    
+"""Implement a function recursivly to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the 
+iterative code in the instructions."""
+
+def get_fib(position):
+    if position == 0: return 0
+    if position == 1: return 1
+    if position > 1: return get_fib(position - 2) + get_fib(position - 1)
+    return -1
+
+# Test cases
+print "asdfdsafgdagad"
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
