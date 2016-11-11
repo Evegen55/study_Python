719d5f25956cb36623fdb3fd8df0e08df65200d8
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-11-11 13:47:21 +0300

    start working

---
+++ src/Udactiy/ud513/QuickSort.py
@@ -0,0 +1,10 @@
+"""
+Created on Nov 11, 2016
+Implement quick sort in Python.
+Input a list.
+Output a sorted list."""
+def quicksort(array):
+    return []
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print quicksort(test)
