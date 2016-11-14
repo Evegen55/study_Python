5bef9d28a8afc38e178e93973a9e628dc5e472c1
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-11-14 18:47:39 +0300

    http://www.tutorialspoint.com/python/python_networking.htm

---
+++ src/ForRasbberryPi/Simple_Client.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 14, 2016
+
+@author: Evgenii_Lartcev
+'''
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
