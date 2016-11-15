8862935a19e5e67d955131d423e5802cdb10d443
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-11-15 18:17:51 +0300

    add results

--- src/Udactiy/ud513/QuickSort.py
+++ src/Udactiy/ud513/QuickSort.py
@@ -40,3 +40,24 @@ def quicksort(array):
        
 test = [21, 4, 1, 3, 8, 20, 25, 6, 21, 14]
 print quicksort(test)
+
+#RESULT OF WORK
+#before partitioning [21, 4, 1, 3, 8, 20, 25, 6, 21, 14] index pivot:  9 index end:  9 index start:  0
+#after partitioning [6, 4, 1, 3, 8, 14, 25, 20, 21, 21] i:  5 index pivot:  9 index end:  9 index start:  0
+
+#before partitioning [6, 4, 1, 3, 8, 14, 25, 20, 21, 21] index pivot:  4 index end:  4 index start:  0
+#after partitioning [6, 4, 1, 3, 8, 14, 25, 20, 21, 21] i:  4 index pivot:  4 index end:  4 index start:  0
+
+#before partitioning [6, 4, 1, 3, 8, 14, 25, 20, 21, 21] index pivot:  3 index end:  3 index start:  0
+#after partitioning [1, 3, 4, 6, 8, 14, 25, 20, 21, 21] i:  1 index pivot:  3 index end:  3 index start:  0
+
+#before partitioning [1, 3, 4, 6, 8, 14, 25, 20, 21, 21] index pivot:  3 index end:  3 index start:  2
+#after partitioning [1, 3, 4, 6, 8, 14, 25, 20, 21, 21] i:  3 index pivot:  3 index end:  3 index start:  2
+
+#before partitioning [1, 3, 4, 6, 8, 14, 25, 20, 21, 21] index pivot:  9 index end:  9 index start:  6
+#after partitioning [1, 3, 4, 6, 8, 14, 21, 20, 21, 25] i:  8 index pivot:  9 index end:  9 index start:  6
+
+#before partitioning [1, 3, 4, 6, 8, 14, 21, 20, 21, 25] index pivot:  7 index end:  7 index start:  6
+#after partitioning [1, 3, 4, 6, 8, 14, 20, 21, 21, 25] i:  6 index pivot:  7 index end:  7 index start:  6
+
+#[1, 3, 4, 6, 8, 14, 20, 21, 21, 25]
