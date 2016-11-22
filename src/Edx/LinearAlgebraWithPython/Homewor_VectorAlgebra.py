bd0a57aaad532f03977a2e4fbe97fcaeb8e61f3d
Evgenii Lartcev <evgenii_lartcev@epam.com>
2016-11-22 20:58:08 +0300

    Length and Normalized Vector - successful

--- src/Edx/LinearAlgebraWithPython/Homewor_VectorAlgebra.py
+++ src/Edx/LinearAlgebraWithPython/Homewor_VectorAlgebra.py
@@ -23,8 +23,23 @@ def vector_addition2():
     print col_vec3
 
 def length_and_normalized_vector():
+    print "length_vector"
     col_vec1 = np.array([[4], [8], [-4]])
-    pass
+    rise_power = np.power(col_vec1, 2)
+    #print rise_power
+    to_sum = np.sum(np.power(col_vec1, 2))
+    #print to_sum
+    exactly = np.sqrt(to_sum)
+    #print exactly
+    rounded = np.round(exactly, 2)
+    print rounded
+    
+    print "normalized_vector"
+    normalized_vector =  col_vec1 / rounded    
+    print normalized_vector
+    rouded_norm_vector = np.round(normalized_vector, 2)
+    print rouded_norm_vector   
 
 vector_addition1()
 vector_addition2()
+length_and_normalized_vector()
