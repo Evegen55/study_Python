063ba9a1c322f7f99daacc1bcdd461ee0141270b
Evgenii Lartcev <evgenii_lartcev@epam.com>
2017-01-14 19:08:45 +0300

    add

---
+++ src/ForRasbberryPi/Blink1.py
@@ -0,0 +1,12 @@
+'''
+Created on 28 дек. 2016 г.
+'''
+import Rpi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+while True:
+    GPIO.output(13, True)
+    time.sleep(1)
+    GPIO.output(13, True)
+    time.sleep(1)
