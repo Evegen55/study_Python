79cb2551e5f7bda9d80ddd76ee161a63d40f82c0
Evgenii Lartcev <evgenii_lartcev@epam.com>
2017-01-14 21:02:58 +0300

    working with rasp

--- src/for_GPIO/Blink1.py
+++ src/for_GPIO/Blink1.py
@@ -1,18 +1,23 @@
 '''
 Created on 28.12.2016
 '''
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
 
 # Pin Definiton:
 ledPin = 16 # P1 pin 16 (Broadcom pin 23)
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ledPin, GPIO.OUT)
+
 # Initial state for LEDs:
 GPIO.output(ledPin, GPIO.LOW)
-while True:
-    GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(ledPin, GPIO.HIGH)
-    time.sleep(1)
+print("Here we go! Press CTRL+C to exit")
+try:
+    while True:
+        GPIO.output(ledPin, True)
+        time.sleep(1)
+        GPIO.output(ledPin, True)
+        time.sleep(1)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup() # cleanup all GPIO
