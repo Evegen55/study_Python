1b46d5a2472559672dc1b4f8447dbfab6bed9ac6
Evgenii Lartcev <evgenii_lartcev@epam.com>
2017-01-14 21:01:46 +0300

    work with rasp

--- src/network/Simple_Client.py
+++ src/network/Simple_Client.py
@@ -9,6 +9,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
+#host = socket.gethostbyaddr('192.168.0.103/24')
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
