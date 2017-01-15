91a116511dba92ade82262200da57fde9b2b963a
Evgenii Lartcev <evgenii_lartcev@epam.com>
2017-01-15 17:19:07 +0300

    it works fine

--- src/for_GPIO/Blink1.py
+++ src/for_GPIO/Blink1.py
@@ -5,19 +5,24 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin Definiton:
-ledPin = 16 # P1 pin 16 (Broadcom pin 23)
+ledPin1 = 16 # P1 pin 16 (Broadcom pin 23)
+ledPin2 = 18 # P1 pin 18 (Broadcom pin 24)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(ledPin1, GPIO.OUT)
+GPIO.setup(ledPin2, GPIO.OUT)
 
 # Initial state for LEDs:
-GPIO.output(ledPin, GPIO.LOW)
+GPIO.output(ledPin1, GPIO.LOW)
+GPIO.output(ledPin2, GPIO.HIGH)
 print("Here we go! Press CTRL+C to exit")
 try:
     while True:
-        GPIO.output(ledPin, True)
+        GPIO.output(ledPin2, False)
+        GPIO.output(ledPin1, True)
         time.sleep(1)
-        GPIO.output(ledPin, True)
+        GPIO.output(ledPin2, True)
+        GPIO.output(ledPin1, False)
         time.sleep(1)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     GPIO.cleanup() # cleanup all GPIO
