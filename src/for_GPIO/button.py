0bfbe7e99da16458873111a7977d2851869bc05f
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-01-16 17:52:54 +0300

    add two other examples

---
+++ src/for_GPIO/button.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 16, 2017
+
+@author: MagPI52
+'''
+from gpiozero import LED, Button
+from signal import pause
+led = LED(17)
+button = Button(21)
+button.when_pressed = led.on
+button.when_released = led.off
+pause()
