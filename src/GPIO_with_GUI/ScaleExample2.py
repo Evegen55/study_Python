c6d95f1f1a5ed05602266e19627e5efe5e6cbb34
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-01-26 16:21:55 +0300

    try GUI and GPIO

--- src/GPIO_with_GUI/ScaleExample2.py
+++ src/GPIO_with_GUI/ScaleExample2.py
@@ -4,20 +4,16 @@ Created on Jan 26, 2017
 @author: Evgenii_Lartcev
 '''
 from Tkinter import *
-#import RPi.GPIO as GPIO
-import time
 
-# GUI definition
+#GUI definition
 root = Tk()
 var = DoubleVar()
-# Pin Definitons:
-#pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
-#pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
 
 def update(duty):
     print var.get()
-    #pwm.ChangeDutyCycle(float(duty))
+    
 w = Scale(root, from_= 0, to = 100, orient = HORIZONTAL, length = 300, width = 50, variable = var, command = update)
 
+#Start GUI
 w.pack()
 root.mainloop()
