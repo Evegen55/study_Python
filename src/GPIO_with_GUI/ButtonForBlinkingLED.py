7f8012612b85d32dd82e71a16f5c899d14fe7c9b
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-01-26 16:38:25 +0300

    try it

--- src/GPIO_with_GUI/ButtonForBlinkingLED.py
+++ src/GPIO_with_GUI/ButtonForBlinkingLED.py
@@ -4,19 +4,21 @@ Created on Jan 26, 2017
 @author: Evgenii_Lartcev
 '''
 from Tkinter import *
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 # GUI definition
 root = Tk()
 var = DoubleVar()
 # Pin Definitons:
-#pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
-#pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
 
 def update(duty):
-    print var.get()
-    #pwm.ChangeDutyCycle(float(duty))
+    pwm.ChangeDutyCycle(float(duty))
+    
 w = Scale(root, from_= 0, to = 100, orient = HORIZONTAL, length = 300, width = 50, variable = var, command = update)
 
 w.pack()
