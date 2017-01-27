bae26cdd4c3284a0647e89ddbe449af0350b1a79
Evgenii Lartcev <evgenii_lartcev@epam.com>
2017-01-27 23:14:34 +0300

    improved and worked code

--- src/GPIO_with_GUI/ButtonForBlinkingLED.py
+++ src/GPIO_with_GUI/ButtonForBlinkingLED.py
@@ -14,12 +14,18 @@ var = DoubleVar()
 pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
 # Pin Setup:
 GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+GPIO.setup(pwmPin, GPIO.OUT)
+#PWM setup
+pwm = GPIO.PWM(pwmPin, 1)  # Initialize PWM on pwmPin 1Hz frequency
+pwm.start(90) #start pwm with duty 90
 
+#update pwm for blinking LED
 def update(duty):
     pwm.ChangeDutyCycle(float(duty))
-    
+    pwm.ChangeFrequency(float(duty)/10)   
+   
 w = Scale(root, from_= 0, to = 100, orient = HORIZONTAL, length = 300, width = 50, variable = var, command = update)
-
 w.pack()
 root.mainloop()
+
+GPIO.cleanup() # cleanup all GPIO after close Scale
