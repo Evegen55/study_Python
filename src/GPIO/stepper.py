3cd49cf0231f55490038b7e9dc92b409ca41ec5a
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-01-31 16:39:12 +0300

    try it on LED before stepper motors

--- src/GPIO/stepper.py
+++ src/GPIO/stepper.py
@@ -8,18 +8,19 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+# Pin Definitons:
 enable_pin = 18
 coil_A_1_pin = 4
 coil_A_2_pin = 17
 coil_B_1_pin = 23
 coil_B_2_pin = 24
 
+# Pin Setup:
 GPIO.setup(enable_pin, GPIO.OUT)
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
-
 GPIO.output(enable_pin, 1)
 
 def forward(delay, steps):  
@@ -43,7 +44,6 @@ def backwards(delay, steps):
         time.sleep(delay)
         setStep(1, 0, 1, 0)
         time.sleep(delay)
-
   
 def setStep(w1, w2, w3, w4):
     GPIO.output(coil_A_1_pin, w1)
@@ -51,9 +51,20 @@ def setStep(w1, w2, w3, w4):
     GPIO.output(coil_B_1_pin, w3)
     GPIO.output(coil_B_2_pin, w4)
 
-while True:
-    delay = raw_input("Delay between steps (milliseconds)?")
-    steps = raw_input("How many steps forward? ")
-    forward(int(delay) / 1000.0, int(steps))
-    steps = raw_input("How many steps backwards? ")
-    backwards(int(delay) / 1000.0, int(steps))
+def clearGPIO():
+    GPIO.cleanup()
+
+def turn_stepper_from_CLI():
+    print("Here we go! Press CTRL+C to exit")
+    try:
+        while True:
+            delay = raw_input("Delay between steps (milliseconds)?")
+            steps = raw_input("How many steps forward? ")
+            forward(int(delay) / 1000.0, int(steps))
+            steps = raw_input("How many steps backwards? ")
+            backwards(int(delay) / 1000.0, int(steps))
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup() # cleanup all GPIO
+        
+#do it from command line interface:
+#turn_stepper_from_CLI()
---
+++ src/GPIO_with_GUI/ButtonControlForStepper.py
@@ -0,0 +1,51 @@
+'''
+Created on Jan 31, 2017
+
+@author: Evgenii_Lartcev
+'''
+from Tkinter import *
+from GPIO.stepper import *
+
+#GUI definition
+root = Tk()
+w = 800 # width for the Tk root
+h = 600 # height for the Tk root
+varDelay = DoubleVar()
+
+def center_window_with_dimensions(width=300, height=200):
+    # get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    # calculate position x and y coordinates
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+def updateAndPritValues(duty):
+    print varDelay.get()
+
+#for 1 step with delay from scale    
+def rotate_left():
+    #print varDelay.get() #for test purpose
+    backwards(varDelay, 1)
+    
+#for 1 step with delay from scale 
+def rotate_right():
+    #print varDelay.get() #for test purpose
+    forward(varDelay, 1)
+
+#Buttons
+button_rotate_left = Button(root, text="Turn stepper motor left", width=30,height=5, bg="white",fg="black", command = rotate_left)
+button_rotate_right = Button(root, text="Turn stepper motor right", width=30,height=5, bg="white",fg="black", command = rotate_right)
+ 
+#Scales   
+scaleDelay = Scale(root, from_= 0, to = 100, orient = HORIZONTAL, length = 300, width = 50, variable = varDelay, command = updateAndPritValues)
+
+# add stuff to the main board
+button_rotate_left.pack(side = 'left')
+button_rotate_right.pack(side = 'right')
+scaleDelay.pack(side = 'bottom')
+# start GUI
+center_window_with_dimensions(w, h)
+root.mainloop()
+clearGPIO() # cleanup all GPIO using incapsulating function from GPIO.stepper
